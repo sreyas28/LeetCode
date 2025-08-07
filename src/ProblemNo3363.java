a292484527799c599dc20d518dce02b374b3200f
Sreyas <sharmasre44+git@gmail.com>
2025-08-07 13:20:15 +0530

    updated ProblemNo3363

--- src/ProblemNo3363.java
+++ src/ProblemNo3363.java
@@ -6,7 +6,15 @@ public class ProblemNo3363 {
 
         ProblemNo3363.Solution a = new ProblemNo3363().new Solution();
         System.out.println(a.maxCollectedFruits(new int[][]
-                        {{1,2,3,4}, {5,6,8,7}, {9,10,11,12}, {13,14,15,16}}
+                        {       {4,2,1,15,20,6,1,3,11},
+                                {1,13,19,18,15,13,6,14,13},
+                                {20,12,10,8,7,12,20,5,17},
+                                {18,9,15,9,16,6,13,1,18},
+                                {12,19,12,7,1,15,16,13,9},
+                                {17,19,15,17,16,11,14,4,15},
+                                {10,16,7,4,8,19,13,2,10},
+                                {1,2,14,6,0,13,11,5,16},
+                                {16,17,12,8,2,12,19,4,7}}
                 ));
 
     }
@@ -15,48 +23,42 @@ public class ProblemNo3363 {
         public int maxCollectedFruits(int[][] fruits) {
             int sum = 0, n = fruits.length;
 
+            int[][] DP = new int[n][n];
+            DP[n-1][0] = fruits[n-1][0];
+
             // top left corner man
             for(int i = 0; i < n; i++) sum += fruits[i][i];
 
-            // top right corner man
-            int[] position = {0, n-1};
-            sum += fruits[position[0]][position[1]];
-
-            for(int i = 1; i < n - 1; i++){
-                int[] tempPosition = new int[2];
-                int max = 0;
-
-                for(int j = -1; j <= 1; j++){
-                    int val = position[1] + j;
-                    if(i < val && val < n && fruits[i][val] > max){
-                        tempPosition[0] = i;
-                        tempPosition[1] = val;
-                        max = fruits[i][val];
-                    }
-                }
-                position = tempPosition;
-                sum += fruits[position[0]][position[1]];
-            }
-
             //bottom left corner man
-            position = new int[] {n-1, 0};
-            sum += fruits[position[0]][position[1]];
-
-            for(int i = 1; i < n - 1; i++){
-                int[] tempPosition = new int[2];
-                int max = 0;
-
-                for(int j = -1; j <= 1; j++){
-                    int val = position[0] + j;
-                    if(i < val && val < n && fruits[val][i] > max){
-                        tempPosition[0] = val;
-                        tempPosition[1] = i;
-                        max = fruits[val][i];
+            for(int col = 1; col < n-1; col++){
+                for(int row = n-1; row >= (n-1-col); row-- ){
+                    // {row}{col-1} && row+i >= n-col && row+i < n
+                    for(int i = -1; i <= 1; i++ ){
+                        int val = row + i;
+                        if(val >= n-col && val < n){
+                            DP[row][col] = Math.max(DP[row][col], fruits[row][col] + DP[val][col-1]);
+                        }
                     }
                 }
-                position = tempPosition;
-                sum += fruits[position[0]][position[1]];
             }
+            sum += DP[n-1][n-2];
+
+            //top right corner man
+            DP = new int[n][n];
+            DP[0][n-1] = fruits[0][n-1];
+
+            for(int row = 1; row < n-1; row++){
+                for(int col = n-1; col >= n-1-row; col--){
+                    for(int i=-1; i<=1; i++){
+                        int val = col + i;
+                        if(val >= n-row && val < n){
+                            DP[row][col] = Math.max(DP[row][col], fruits[row][col] + DP[row-1][val]);
+                        }
+                    }
+                }
+            }
+            sum += DP[n-2][n-1];
+
 
             return sum;
         }
