603f572ae6d3c7a564ef55ba5b10f8c79f4072a7
Sreyas <sharmasre44+git@gmail.com>
2025-08-10 02:42:37 +0530

    updated ProblemNo231

--- src/ProblemNo231.java
+++ src/ProblemNo231.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class ProblemNo231 {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public boolean isPowerOfTwo(int n) {
+            if(n <= 0) return false;
+            while(n != 1){
+                if(n % 2 != 0) return false;
+                n /= 2;
+            }
+            return true;
+        }
+    }
+
 }
