613a56223799079627b2ca2f746218b0ab2c639d
Sreyas <sharmasre44+git@gmail.com>
2025-08-21 19:54:29 +0530

    updated ProblemNo2348

--- src/ProblemNo2348.java
+++ src/ProblemNo2348.java
@@ -10,26 +10,41 @@ public class ProblemNo2348 {
 
     class Solution {
         public long zeroFilledSubarray(int[] nums) {
-            ArrayList<Integer> zeroes = new ArrayList<>();
-
-            int count_temp = 0;
-            for(int num: nums){
-                if (num == 0) count_temp++;
-                else {
-                    zeroes.add(count_temp);
-                    count_temp = 0;
-                }
-            }
-            zeroes.add(count_temp);
-
             long result = 0;
+            int count_temp = 0;
 
-            for(int val: zeroes){
-                result += (long) val *(val+1)/2;
+            for(int num: nums){
+                if (num == 0) {
+                    count_temp++;
+                    result += count_temp;
+                }
+                else count_temp = 0;
             }
-
             return result;
         }
     }
 
 }
+
+
+//public long zeroFilledSubarray(int[] nums) {
+//    ArrayList<Integer> zeroes = new ArrayList<>();
+//
+//    int count_temp = 0;
+//    for(int num: nums){
+//        if (num == 0) count_temp++;
+//        else {
+//            zeroes.add(count_temp);
+//            count_temp = 0;
+//        }
+//    }
+//    zeroes.add(count_temp);
+//
+//    long result = 0;
+//
+//    for(int val: zeroes){
+//        result += (long) val *(val+1)/2;
+//    }
+//
+//    return result;
+//}
