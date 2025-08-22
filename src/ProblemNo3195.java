beecd4dc3512f1af1c96f06035a3bfa4bf5df1d5
Sreyas <sharmasre44+git@gmail.com>
2025-08-22 12:53:49 +0530

    ProblemNo3195

---
+++ src/ProblemNo3195.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class ProblemNo3195 {
+    public static void main(String[] args) {
+
+        ProblemNo3195.Solution a = new ProblemNo3195().new Solution();
+        System.out.println(a.minimumArea(new int[][]{
+                {0,1,0},{1,0,1}
+        }));
+
+    }
+
+    class Solution {
+        public int minimumArea(int[][] grid) {
+
+            int[] minMax = {Integer.MAX_VALUE, Integer.MIN_VALUE};
+
+            for(int i=0; i< grid.length; i++){
+                for (int j=0; j< grid[0].length; j++){
+                    if(grid[i][j] == 1){
+                        minMax[0] = Math.min(minMax[0], j);
+                        minMax[1] = Math.max(minMax[1], j);
+                    }
+                }
+            }
+
+            int result = minMax[1] - minMax[0] + 1;
+            System.out.println(Arrays.toString(minMax));
+
+            minMax = new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE};
+
+            for(int i=0; i< grid[0].length; i++){
+                for (int j=0; j< grid.length; j++){
+                    if(grid[j][i] == 1){
+                        minMax[0] = Math.min(minMax[0], j);
+                        minMax[1] = Math.max(minMax[1], j);
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(minMax));
+
+            result *= (minMax[1]-minMax[0]) + 1;
+
+            return result;
+        }
+    }
+
+}
