f832c774baa5f106674549071ede6bb6e1836d17
Sreyas <sharmasre44+git@gmail.com>
2025-10-11 12:10:12 +0530

    ProblemNo2486

---
+++ src/ProblemNo2486.java
@@ -0,0 +1,19 @@
+public class ProblemNo2486 {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public int appendCharacters(String s, String t) {
+            int j = 0;
+            for(int i=0; i<s.length(); i++){
+                if(s.charAt(i) == t.charAt(j)) {
+                    j++;
+                    if(j >= t.length()) return 0;
+                }
+            }
+            return t.length() - j;
+        }
+    }
+
+}
