993c81ec504fb2465b2f815a3c6ec3e56f54593e
Sreyas <sharmasre44+git@gmail.com>
2026-04-05 08:13:03 +0530

    ProblemNo657

---
+++ src/ProblemNo657.java
@@ -0,0 +1,31 @@
+public class ProblemNo657 {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public boolean judgeCircle(String moves) {
+            int x = 0, y = 0;
+
+            for (char c : moves.toCharArray()) {
+                switch (c){
+                    case 'U':
+                        y++;
+                        break;
+                    case 'D':
+                        y--;
+                        break;
+                    case 'L':
+                        x--;
+                        break;
+                    case 'R':
+                        x++;
+                        break;
+                }
+            }
+
+            return x == 0 && y == 0;
+        }
+    }
+
+}
