aac82355aa48a5046580070c1d21ee36db141f26
Sreyas <sharmasre44+git@gmail.com>
2026-05-29 07:07:21 +0530

    ProblemNo3300

---
+++ src/ProblemNo3300.java
@@ -0,0 +1,21 @@
+public class ProblemNo3300 {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public int minElement(int[] nums) {
+            int min = Integer.MAX_VALUE;
+            for(int i: nums){
+                int sum = 0;
+                while(i > 0){
+                    sum += (i%10);
+                    i /= 10;
+                }
+                min = Math.min(min, sum);
+            }
+            return min;
+        }
+    }
+
+}
