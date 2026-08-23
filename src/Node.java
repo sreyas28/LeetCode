435f8643a1c4c272a567b0fbd3f9f5f42d08ef00
Sreyas <sharmasre44+git@gmail.com>
2026-08-23 23:54:57 +0530

    updated

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,5 @@
+import java.util.prefs.NodeChangeEvent;
+
 class Node {
     int val;
     Node next;
@@ -8,4 +10,16 @@ class Node {
         this.next = null;
         this.random = null;
     }
+
+    public void print(Node node){
+
+        while(node != null){
+            System.out.print("[" + node.val + ", " + node.random + ", " + node + "]" + " ----> ");
+            node = node.next;
+        }
+
+        System.out.print("NULL");
+
+    }
+
 }
