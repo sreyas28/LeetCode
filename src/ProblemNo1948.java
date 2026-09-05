95f90d5a903768f8d15b812d8a217e99e1ace3ee
Sreyas <sharmasre44+git@gmail.com>
2026-09-05 09:18:26 +0530

    ProblemNo1948

--- src/ProblemNo1948.java
+++ src/ProblemNo1948.java
@@ -8,16 +8,108 @@ public class ProblemNo1948 {
         ProblemNo1948.Solution a = new ProblemNo1948().new Solution();
 
         List<List<String>> paths = new ArrayList<>();
-        String[][] pat = {{"a"},{"c"},{"d"},{"a", "b"},{"c", "b"},{"d", "a"}};
-        for(String[] p: pat) paths.add(new ArrayList<>(List.of(p)));
+//        String[][] pat = {{"a"}, {"c"}, {"d"}, {"a", "b"}, {"c", "b"}, {"d", "a"}};
+        String[][] pat = {{"y"}, {"y", "b"}, {"y", "b", "a"}, {"y", "d"}, {"y", "d", "c"}, {"z"}, {"z", "d"}, {"z", "d", "c"}, {"z", "d", "c", "b"}, {"z", "d", "c", "b", "a"}};
+//        String[][] pat = {{"f","r","g"},{"f","o","x","t"},{"f","o","x","d"},{"f","o","l"},{"l","q"},{"h","t"},{"h","o","d"},{"h","o","t"}};
+        for (String[] p : pat) paths.add(new ArrayList<>(List.of(p)));
 
         System.out.println(a.deleteDuplicateFolder(paths));
     }
 
+
     class Solution {
+        private class TrieNode {
+            String serial;
+            Map<String, TrieNode> children;
+
+            TrieNode(String serial) {
+                this.serial = serial;
+                this.children = new TreeMap<>();
+            }
+        }
+
+        private Map<String, List<TrieNode>> matchedSubFolder;
+
+        public List<List<String>> deleteDuplicateFolder(List<List<String>> paths) {
+            TrieNode root = new TrieNode("");
+            matchedSubFolder = new HashMap<>();
+
+            for (List<String> path : paths) {
+                TrieNode node = root;
+                for (String str : path) {
+                    node = node.children.computeIfAbsent(str, k -> new TrieNode(str));
+                }
+            }
+
+            inorderTraversal(root);
+            for (String key : matchedSubFolder.keySet()) {
+                if (matchedSubFolder.get(key).size() <= 1) continue;
+                String[] toRemove = key.split(",");
+
+                boolean removeParent = true;
+                for (TrieNode node : matchedSubFolder.get(key)) {
+
+                    for (String str : toRemove) node.children.remove(str);
+                    if (!node.children.isEmpty()) removeParent = false;
+                }
+                if (removeParent) {
+                    for (TrieNode node : matchedSubFolder.get(key)) node.serial = null;
+                }
+            }
+
+            List<List<String>> res = new ArrayList<>();
+            for (TrieNode child : root.children.values()) {
+                if (child.serial == null) continue;
+                construct(child, res, new ArrayList<>());
+            }
+            return res;
+        }
+
+        private String inorderTraversal(TrieNode node) {
+            StringBuilder sb = new StringBuilder();
+
+            if (node.children.size() > 1) {
+                for (String str : node.children.keySet()) {
+                    String path = inorderTraversal(node.children.get(str));
+                    //matchedSubFolder.computeIfAbsent(path, x -> new ArrayList<>()).add(node);
+                    sb.append(path).append(",");
+                }
+            }
+            else if (node.children.size() == 1) sb.append(inorderTraversal(node.children.values().iterator().next()));
+
+
+            if (!sb.isEmpty()) matchedSubFolder.computeIfAbsent(sb.toString(), x -> new ArrayList<>()).add(node);
+            sb.append(node.serial).append(",");
+            return sb.toString();
+        }
+
+        private void construct(TrieNode node, List<List<String>> paths, List<String> currentPath) {
+            currentPath.add(node.serial);
+            paths.add(currentPath);
+
+            for (TrieNode child : node.children.values()) {
+                if (child.serial == null) continue;
+                construct(child, paths, new ArrayList<>(currentPath));
+            }
+        }
+
+        private void printer(TrieNode node) {
+            if (node.children.isEmpty()) {
+                System.out.print(node.serial + ", ");
+                return;
+            }
+
+            System.out.print("{ " + node.serial + " ->{");
+            for (String key : node.children.keySet()) {
+                printer(node.children.get(key));
+            }
+            System.out.print("}, ");
+        }
+    }
+
+    class Solution_ {
 
         class Trie {
-
             String serial; // current node structure's serialized representation
             Map<String, Trie> children = new HashMap<>(); // current node's child nodes
         }
@@ -68,7 +160,8 @@ public class ProblemNo1948 {
                 List<String> path,
                 List<List<String>> ans
         ) {
-            if (freq.getOrDefault(node.serial, 0) > 1) return; // if the serialization representation appears more than once, it needs to be deleted
+            if (freq.getOrDefault(node.serial, 0) > 1)
+                return; // if the serialization representation appears more than once, it needs to be deleted
 
             if (!path.isEmpty()) {
                 ans.add(new ArrayList<>(path));
