a0a5b7b002a280c48c84613866adb8abbe56c1d0
Sreyas <sharmasre44+git@gmail.com>
2026-09-08 07:17:03 +0530

    ProblemNo3870

---
+++ src/ProblemNo3870.java
@@ -0,0 +1,13 @@
+public class ProblemNo3870 {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public int countCommas(int n) {
+            int count = n - 999;
+            return count > 0 ? count: 0;
+        }
+    }
+
+}
